d2e09cbed931c75714a8b3853196b8700b146ce2
khalilsleimi <intrigue.sh@gmail.com>
2026-01-13 10:52:22 +0100

    commit most changes

---
+++ HVault/app/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner printSecrets(
+      @Value("${db.username:NOT_FOUND}") String dbUser,
+      @Value("${db.password:NOT_FOUND}") String dbPass,
+      @Value("${ssh.host:NOT_FOUND}") String sshHost,
+      @Value("${ssh.port:NOT_FOUND}") String sshPort
+  ) {
+    return args -> {
+      System.out.println("=== Values read from Vault ===");
+      System.out.println("db.username = " + dbUser);
+      System.out.println("db.password = " + dbPass);
+      System.out.println("ssh.host    = " + sshHost);
+      System.out.println("ssh.port    = " + sshPort);
+      System.out.println("==============================");
+    };
+  }
+}
